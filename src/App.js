7fd67331cbdac033945e63074c156f69dad869cb
victory-lydia <vickylydy@gmail.com>
2022-10-04 22:32:25 +0100

    I created a counter using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import  { useEffect, useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css';
+function App(){
+  const [counter, setCounter] = useState(0);
+  let[incremented, setIncremented] = useState(false);
+
+  /*
+  function incrementCounter() {
+    setCounter(counter + 1)
+    setIncremented(true)
+    setCounter(counter => counter + 1)
+*/
+
+    const automatic = () => {
+      let interval = setInterval(() => {
+        setCounter(counter => {
+          if(counter === 60) {
+            clearInterval(interval)
+            return counter
+          }
+         return counter + 1
+        })
+      }, 1000)
+    }
+
+    useEffect(() => {
+      automatic()
+    }, [])
+
+
+  function decrementCounter() {
+    if(counter<= 0){
+      setCounter(65)
+    } else if (incremented === true){
+      setCounter(65)
+    }
+    setCounter(counter => counter-5)
+    setIncremented(false);
+  }
+  return(
+    <div className='counter'> 
+      <h1>{counter}</h1>
+      <button className='decrementBtn' onClick={decrementCounter}>DECREMENT</button>
     </div>
-  );
+  )
 }
+
 
 export default App;
